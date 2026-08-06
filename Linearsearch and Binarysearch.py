13337a7dc96cfa512f6f10a108fe8a5cabe058d3
Anvesh-1821 <gangaraboinaanvesh18@gmail.com>
2026-08-06 20:06:27 +0530

    Update Linearsearch and Binarysearch.py

--- Linearsearch and Binarysearch.py
+++ Linearsearch and Binarysearch.py
@@ -22,15 +22,37 @@ def linear_search(arr, n, key):
     return -1
 
 
+# Main Program
 arr = [10, 20, 30, 40, 50]
 
 # Binary Search
 key1 = 40
 result1 = binary_search(arr, key1)
-print("Binary Search: Element found at index", result1)
+
+if result1 != -1:
+    print("Binary Search: Element found at index", result1)
+else:
+    print("Binary Search: Element not found")
+
+print("Time Complexity of Binary Search:")
+print("Best Case    : O(1)")
+print("Average Case : O(log n)")
+print("Worst Case   : O(log n)")
+print("Space Complexity: O(1)\n")
+
 
 # Linear Search
 key2 = 20
 n = len(arr)
 result2 = linear_search(arr, n, key2)
-print("Linear Search: Element found at index", result2)
+
+if result2 != -1:
+    print("Linear Search: Element found at index", result2)
+else:
+    print("Linear Search: Element not found")
+
+print("Time Complexity of Linear Search:")
+print("Best Case    : O(1)")
+print("Average Case : O(n)")
+print("Worst Case   : O(n)")
+print("Space Complexity: O(1)")
